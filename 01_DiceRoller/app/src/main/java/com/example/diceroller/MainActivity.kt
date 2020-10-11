4569b8e775a078ecdf312b4aef1ce04e7ebdee0d
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2020-10-11 18:14:37 +0200

    Implemented rolling random numbers (Step 3 in udacity naming)

--- 01_DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 01_DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.roll_button).run { setOnClickListener {rollDice()} }
+    }
+
+    private fun rollDice() {
+        findViewById<TextView>(R.id.roll_result_text).run { text = (Random().nextInt(6)+1).toString() }
     }
 }
