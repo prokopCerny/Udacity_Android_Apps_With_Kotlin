28cf244459fc0b487759afabe0a04371b3e8f771
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2020-10-18 18:20:59 +0200

    Added Nickname EditText and onclick event to save it

--- 02_AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ 02_AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val nickname = findViewById<EditText>(R.id.nickname_edit).apply {
+            visibility = View.GONE
+        }.text.toString()
+
+        with(findViewById<TextView>(R.id.nickname_text)) {
+            text = nickname
+            visibility = View.VISIBLE
+        }
+
+        view.visibility = View.GONE
+
+        with(getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager) {
+            hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
