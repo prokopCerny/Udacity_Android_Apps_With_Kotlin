f479d2cc0398f387532fd5dfac5b5b0e444ff87e
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2020-10-18 20:24:41 +0200

    Add three colouring buttons

--- 02_ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ 02_ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -20,7 +20,10 @@ class MainActivity : AppCompatActivity() {
             box_three_text,
             box_four_text,
             box_five_text,
-            contraint_layout
+            contraint_layout,
+            red_button,
+            yellow_button,
+            green_button
         ).forEach { it.setOnClickListener { it2 -> makeColoured(it2) } }
     }
 
@@ -33,6 +36,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
