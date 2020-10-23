eda0f12f0f449a453a000137db5e029006186248
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2020-10-23 14:50:42 +0200

    Added actions to remaining buttons, added back stack manipulation

--- 03_AndroidTrivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ 03_AndroidTrivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,14 +22,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGameOverBinding>(
+                inflater, R.layout.fragment_game_over, container, false).apply {
+            tryAgainButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_gameOverFragment_to_gameFragment))
+        }
         return binding.root
     }
 }
