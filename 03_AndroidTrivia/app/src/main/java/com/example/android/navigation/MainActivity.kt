cb6aa56dd5500039a8bfcf019198b1519c7cebf3
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2020-10-23 16:34:59 +0200

    Added navdrawer

--- 03_AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ 03_AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,19 +19,30 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        NavigationUI.setupActionBarWithNavController(this, this.findNavController(R.id.myNavHostFragment))
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment).apply {
+            addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+                drawerLayout.setDrawerLockMode(if (nd.id == nc.graph.startDestination) DrawerLayout.LOCK_MODE_UNLOCKED else DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return this.findNavController(R.id.myNavHostFragment).navigateUp()
+        return NavigationUI.navigateUp(this.findNavController(R.id.myNavHostFragment), drawerLayout)
     }
 }
