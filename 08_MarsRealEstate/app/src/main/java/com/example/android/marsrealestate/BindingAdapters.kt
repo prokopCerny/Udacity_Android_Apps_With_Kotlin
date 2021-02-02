4fd93c669610673bdf93cffe0fd7634c97036abc
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2021-02-02 20:14:14 +0100

    Added loading a grid of images in RecyclerView

--- 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -20,8 +20,11 @@ package com.example.android.marsrealestate
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
 fun ImageView.bindImage(imgUrl: String?) {
@@ -31,4 +34,10 @@ fun ImageView.bindImage(imgUrl: String?) {
                 .load(imgUri).apply(RequestOptions().placeholder(R.drawable.loading_animation).error(R.drawable.ic_broken_image))
                 .into(this)
     }
+}
+
+@BindingAdapter("listData")
+fun RecyclerView.bindRecyclerView(data: List<MarsProperty>?) {
+    val adapter = this.adapter as PhotoGridAdapter
+    adapter.submitList(data)
 }
