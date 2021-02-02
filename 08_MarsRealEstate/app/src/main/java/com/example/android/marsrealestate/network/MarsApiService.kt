dc357f1e2f4ca356e521ad9da53272dcbe290339
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2021-02-02 19:41:30 +0100

    refactor getMarsRealEstateProperties() using coroutines

--- 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -20,7 +20,7 @@ package com.example.android.marsrealestate.network
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -37,7 +37,7 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    fun getProperties(): Deferred<List<MarsProperty>>
 }
 
 object MarsApi {
--- 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ 08_MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,6 +34,9 @@ import retrofit2.Response
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
@@ -49,15 +56,20 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object : Callback<List<MarsProperty>> {
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Succes: ${response.body()?.size} Mars properties retrieved"
+        coroutineScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties().await()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
             }
+        }
+    }
 
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure: ${t.message}"
-            }
-
-        })
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
+
+
