24a8009d9c90f526b8b789e34feee66994d9379e
Prokop Černý <5815801+prokopCerny@users.noreply.github.com>
2021-02-03 18:23:05 +0100

    Add FAB to go to GDG list page

--- 10_Styling/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ 10_Styling/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +23,17 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val binding = HomeFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java).also {
+            binding.viewModel = it
+        }
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate) {
+                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+                viewModel.onNavigatedToSearch()
+            }
+        })
 
-        return view
+        return binding.root
     }
 }
